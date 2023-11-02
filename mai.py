bc80a4dc352dd9b16b0f6e18a3106eecd169ad0c
Kunal Kankariya <kkankariya007@gmail.com>
2023-11-03 01:54:41 +0530

    Backend Completed

--- mai.py
+++ mai.py
@@ -5,22 +5,24 @@ import pandas as pd
 
 app = FastAPI()
 
-# Load the mental health dataset
 df = pd.read_csv("mentahealth.csv")
 
-model_name = "gpt2"  # You can replace this with the model of your choice
+model_name = "gpt2"  
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 model.eval()
 
 @app.post("/answer/")
 def answer_question(context: str):
-    # Tokenize the context and convert it to tensor
     input_ids = tokenizer.encode(context, return_tensors="pt")
-
-    # Generate an answer using the model
     with torch.no_grad():
-        output = model.generate(input_ids,num_return_sequences=1,max_length=40, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
+        output = model.generate(input_ids,num_return_sequences=1,max_length=70, no_repeat_ngram_size=2, top_k=50, top_p=0.95)
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return {"answer": generated_text}
+        first_position = generated_text.find('\n')
+        second_position = generated_text.find('\n', first_position + 1)
+        gen_text=generated_text[second_position+1:]
+        generated_text=gen_text.replace("...","")
+        mtext=generated_text.replace("\n","")
+        last_period_position = mtext.rfind('.')
+        print (generated_text)
+    return mtext[:last_period_position+1]
